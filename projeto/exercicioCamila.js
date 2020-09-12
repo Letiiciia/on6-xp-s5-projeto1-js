a3243df547c40e341074522083fc2237a42ac337
letiiciia <leticialimaferreira2@gmail.com>
2020-09-11 23:04:33 -0300

    subindo exercicios

---
+++ projeto/exercicioCamila.js
@@ -0,0 +1,124 @@
+console.log('--------------------------------------')
+console.log('     Projeto Carrinho de Compras     ')
+console.log('--------------------------------------')
+
+const db = require('./database')
+const { produtos } = db
+
+
+//Exercício 1 (Listar no console a tabela em ordem crescente de preço)
+console.table(produtos)
+
+function ordenarPreco(a, b) {
+  return a.preco - b.preco
+}
+produtos.sort(ordenarPreco)
+
+console.table(produtos)
+
+
+//Exercício 2 (receber as entradas id e quantidade)
+const readline = require('readline-sync')
+const listaDeCompras = []
+const listaSubTotais = []
+const quantidade = []
+
+function comprar() {
+  let entrarId = parseFloat(readline.question("Qual o ID do produto?"))
+
+  // validando ID//
+  while(entrarId <= 0 || entrarId >= 9 ){
+    entrarId = parseFloat(readline.question("Digite novamente o Id..."));
+  }
+
+  let entrarQuantidade = parseFloat(readline.question("Qual a quantidade  que deseja comprar? "))
+  
+  //validando a Quantidade //
+  while(entrarQuantidade <= 0){
+    entrarQuantidade = parseFloat(readline.question("Digite a quantidade..."));
+  }
+  
+  function procurar(produto) {
+     return produto.id === entrarId
+  }
+  const produtoEncontrado = produtos.find(procurar)
+
+  //console.log(produtoEncontrado)
+
+  listaDeCompras.push(produtoEncontrado)
+  listaDeCompras.push(entrarQuantidade)
+
+  const subTotal = produtoEncontrado.preco * entrarQuantidade
+  // console.log(subTotal)
+
+  listaSubTotais.push(subTotal)
+  quantidade.push(entrarQuantidade)
+}
+
+
+let continuarComprando = "S"
+
+while (continuarComprando === "S") {
+  comprar()
+  continuarComprando = (readline.question("Você deseja continuar comprando? (S/N)"))
+
+}
+
+console.table(listaDeCompras)
+
+function somarTodos(acumulador, subTotal) {
+  return acumulador + subTotal
+}
+const somaSubtotais = listaSubTotais.reduce(somarTodos, 0)
+
+const somaSubtotaisReais = somaSubtotais.toFixed(2)
+
+console.log(`Subtotal: R$ ${somaSubtotaisReais}`)
+
+
+// 3. Perguntar se possui cupom de desconto
+let cupomDesconto = parseFloat(readline.question("Possui cupom de desconto?"))
+
+//validando cupom de desconto //
+while(cupomDesconto > 15){
+  cupomDesconto = parseFloat(readline.question("Digite um cupom válido..."));
+}
+
+console.log(`Cupom: ${cupomDesconto}`)
+
+  const desconto = somaSubtotais * (cupomDesconto/100)
+  const descontoReais = desconto.toFixed(2)
+
+  console.log(`Desconto: R$ ${descontoReais}`)
+
+  const valorTotal = somaSubtotais - desconto
+  const valorTotalReais = valorTotal.toFixed(2)
+
+  console.log(`Valor total da compra: R$ ${valorTotalReais}`)
+
+
+
+const hoje = new Date()
+const data1 = hoje.toLocaleDateString("pt-BR")
+
+
+// 4. Criação de uma classe chamada Pedido
+
+const arrayReduzido = quantidade.reduce((acumulador,qtdTotal) => acumulador + qtdTotal, 0)
+
+class Pedido {
+  constructor(arrayReduzido, valorDoCupom, dataDoPedido, listaDeProdutos) {
+    this.qtdItens = arrayReduzido
+    this.cupom = valorDoCupom
+    this.data = dataDoPedido
+    this.compra = listaDeProdutos
+    
+  }
+}
+
+const sacola = new Pedido (arrayReduzido, cupomDesconto, data1, listaDeCompras)
+const { qtdItens, cupom, data, compra} = sacola
+console.table("Produtos adquiridos")
+console.table(sacola.compra)
+console.table("Informações da compra")
+console.table({qtdItens, cupom, data})
--- projeto/index.js
+++ projeto/index.js
@@ -22,23 +22,82 @@ console.log(listaDeCompras)
 
 
 //4
+function comprar(){
+    let idProduto = parseFloat(readline.question('Qual produto deseja? '))
+    while(idProduto <= 0 || idProduto >= 9 ){
+        idProduto = parseFloat(readline.question("Digite novamente o Id "));
+      }
+    
+    let quantidadeDeProduto = parseFloat(readline.question('Quantos itens? '))
+    while(quantidadeDeProduto <= 0){
+        quantidadeDeProduto = parseFloat(readline.question("Digite novamente a quantidade "))
+    }
+
+    const produtoAdquirido = produtos.find((item) => item.id === idProduto)
+
+    const produtoAdicionado = {...produtoAdquirido, quantidade: quantidadeDeProduto}
+    
+    listaDeCompras.push(produtoAdicionado)
+}
 
 
 
 
 let continuarCompra = "S"
 while (continuarCompra === "S") {
-    const idProduto = parseFloat(readline.question('Qual produto deseja? '))
-    const quantidadeDeProduto = parseFloat(readline.question('Quantos itens? '))
-    const produtoAdquirido = produtos.find((item) => item.id === idProduto)
-    listaDeCompras.push(produtoAdquirido)
+    comprar()
     continuarCompra = readline.question('Deseja continuar a compra? (S/N) ')
 }
 console.table(listaDeCompras)
-const listaWhile = []
-const soma = quantidadeDeProduto * produtoAdquirido.preco
 
-console.log(soma)
+let cupomDesconto = parseFloat(readline.question('Voce tem cupom de desconto? '))
+while(cupomDesconto > 15){
+ cupomDesconto = parseFloat(readline.question('Voce tem cupom de desconto? '))
+}
+
+class Pedido {
+constructor(listaDeCompras, valorDoCupom , dataCompra = new Date() ){
+this.listaDeCompras = listaDeCompras
+this.valorDoCupom = valorDoCupom
+this.dataCompra = dataCompra
+this.valorSubtotal = 0
+this.TotalItens = 0
+this.valorDesconto = 0
+this.valorFinal = 0
+}
+somaItens(){
+    this.TotalItens = this.listaDeCompras.reduce((acumulador,produto) => acumulador + produto.quantidade, 0)
+}
+calculaSubTotal(){
+    this.valorSubtotal = this.listaDeCompras.reduce((acumulador,produto) => acumulador + produto.quantidade * produto.preco, 0)
+}
+calcularComCupom(){
+    this.valorDesconto = this.valorSubtotal * (this.valorDoCupom/100)
+}
+calcularValorFinal(){
+    this.valorFinal = this.valorSubtotal - this.valorDesconto
+}
+}
+
+const pedido1 = new Pedido(listaDeCompras, cupomDesconto)
+pedido1.somaItens()
+pedido1.calculaSubTotal()
+pedido1.calcularComCupom()
+pedido1.calcularValorFinal()
+
+
+console.log(`Quantidade de itens: ${pedido1.TotalItens}`)
+console.log(`Valor total: ${pedido1.valorSubtotal.toFixed(2)}`)
+console.log(`Valor do desconto: ${pedido1.valorDesconto.toFixed(2)}`)
+console.log(`Valor final com desconto: ${pedido1.valorFinal.toFixed(2)}`)
+console.log(`Data da compra: ${pedido1.dataCompra}`)
+
+
+
+
+//const listaWhile = []
+//const soma = quantidadeDeProduto * produtoAdquirido.preco
+//console.log(soma)
 
 
 
